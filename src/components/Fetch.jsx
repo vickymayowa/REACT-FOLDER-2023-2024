2f5dab30592bd65cb53bd4da0aec1592cd81d80c
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2023-10-16 08:49:13 +0100

    Working on map

---
+++ src/components/Fetch.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Fetch = () => {
+    const fetchData =()=>{
+        
+    }
+  return (
+    <>
+     <button className="bg-lue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleDelete(index)} > Delete</button>
+    </>
+  )
+}
+
+export default Fetch
